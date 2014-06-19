14bcbf10432fc4aae54390b3f39b3486a6f7acdb
daowangli@gmail.com <daowangli@gmail.com>
2014-06-19 18:09:07 +0800

    #15 Add comment for web service interface

--- src/main/java/edu/zju/bme/clever/service/CleverService.java
+++ src/main/java/edu/zju/bme/clever/service/CleverService.java
@@ -15,6 +15,7 @@ import edu.zju.bme.clever.service.util.MapAdapter;
 public interface CleverService {
 
 	/**
+	 * Set CDR service status to start
 	 * @return
 	 * 0 success
 	 */
@@ -22,6 +23,7 @@ public interface CleverService {
 	int start();
 
 	/**
+	 * Set CDR service status to stop
 	 * @return
 	 * 0 success
 	 */
@@ -29,26 +31,32 @@ public interface CleverService {
 	int stop();
 
 	/**
+	 * Get CDR service status
 	 * @return
+	 * TRUE CDR service status is start
+	 * FALSE CDR service status is stop
 	 */
 	@WebMethod
 	boolean getServiceStatus();
 
 	/**
-     * @param archetypes
-     * @param arms
+	 * Reconfigure CDR service with input archetypes, CDR service must be in stop status
+     * @param archetypes Input archetypes 
+     * @param arms Input archetype relational mappings, not used
 	 * @return
 	 * 0 success
-	 * -1 service running
+	 * -1 service is start
 	 * -2 internal error
 	 */
 	@WebMethod
 	int reconfigure(Collection<String> archetypes, Collection<String> arms);
 
 	/**
-	 * @param aql
+	 * Execute select aql
+	 * @param aql Select archetype query language
 	 * @return
-	 * -1 service running
+	 * null service is stop or internal error
+	 * else results in ODIN format
 	 */
 	@WebMethod
 	List<String> select(String aql);
@@ -57,9 +65,12 @@ public interface CleverService {
 	List<String> selectParameterized(String aql, @XmlJavaTypeAdapter(MapAdapter.class) Map<String, String> parameters);
 
 	/**
-	 * @param aql
+	 * Execute select count aql
+	 * @param aql Select count archetype query language
 	 * @return
-	 * -1 service running
+	 * -1 service is stop
+	 * -2 internal error
+	 * else result count
 	 */
 	@WebMethod
 	long selectCount(String aql);
@@ -68,17 +79,23 @@ public interface CleverService {
 	long selectCountParameterized(String aql, @XmlJavaTypeAdapter(MapAdapter.class) Map<String, String> parameters);
 
 	/**
-	 * @param dadls
+	 * Insert ODIN format archetype instances into CDR service 
+	 * @param dadls ODIN format archetype instances
 	 * @return
-	 * -1 service running
+	 * 0 success
+	 * -1 service is stop
+	 * -2 internal error
 	 */
 	@WebMethod
 	int insert(List<String> dadls);
 
 	/**
-	 * @param aql
+	 * Execute delete aql
+	 * @param aql Delete archetype query language
 	 * @return
-	 * -1 service running
+	 * -1 service is stop
+	 * -2 internal error
+	 * else deleted record count
 	 */
 	@WebMethod
 	int delete(String aql);
@@ -87,9 +104,12 @@ public interface CleverService {
 	int deleteParameterized(String aql, @XmlJavaTypeAdapter(MapAdapter.class) Map<String, String> parameters);
 
 	/**
-	 * @param aql
+	 * Execute update aql
+	 * @param aql Update archetype query language
 	 * @return
-	 * -1 service running
+	 * -1 service is stop
+	 * -2 internal error
+	 * else updated record count
 	 */
 	@WebMethod
 	int update(String aql);
@@ -98,15 +118,42 @@ public interface CleverService {
 	int updateParameterized(String aql, @XmlJavaTypeAdapter(MapAdapter.class) Map<String, String> parameters);
 
 	/**
-	 * @param aql
-	 * @return sql
+	 * Get sql from aql 
+	 * @param aql 
+	 * @return 
+	 * null service is stop or internal error
+	 * else sqls
 	 */
 	@WebMethod
 	List<String> getSQL(String aql);
 
+	/**
+	 * Get archetype ids deployed in CDR service
+	 * @return 
+	 * null service is stop or internal error
+	 * else archetype ids 
+	 */
 	@WebMethod
 	Set<String> getArchetypeIds();
 
+	/**
+	 * Get archetype content string deployed in CDR service
+	 * @param archetypeId
+	 * @return 
+	 * "" service is stop or internal error
+	 * else archetype content string
+	 */
 	@WebMethod
 	String getArchetypeString(String archetypeId);
+
+	/**
+	 * Get archetype content strings deployed in CDR service
+	 * @param archetypeIds Archetype ids
+	 * @return 
+	 * "" service is stop or internal error
+	 * else archetype content strings
+	 */
+	@WebMethod
+	Set<String> getArchetypeStrings(Set<String> archetypeIds);
+	
 }
